c4aad6d562c5fb0f6cceaaa2391eb320f51a3307
Travis CI User <travis@example.org>
2018-11-02 15:47:18 +0000

    Upload documentation for v0.5.0-alpha.3

---
+++ v0.5.0-alpha.3/doc/implementors/core/ops/deref/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["pyo3"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"pyo3/ffi/struct.PyDateTimeAPI.html\" title=\"struct pyo3::ffi::PyDateTimeAPI\">PyDateTimeAPI</a>",synthetic:false,types:["pyo3::ffi::datetime::PyDateTimeAPI"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
