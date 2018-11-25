4455b0f78f2b84d1fac8bfda6b8bca1a70174486
Travis CI User <travis@example.org>
2018-11-25 23:20:41 +0000

    Upload documentation for v0.5.2

---
+++ v0.5.2/doc/implementors/core/iter/iterator/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["pyo3"] = [{text:"impl&lt;'p&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"pyo3/types/struct.PyIterator.html\" title=\"struct pyo3::types::PyIterator\">PyIterator</a>&lt;'p&gt;",synthetic:false,types:["pyo3::types::iterator::PyIterator"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
