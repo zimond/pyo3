e29d23c3e6fbfa5814eae7bf66ac41e76514f4c5
Travis CI User <travis@example.org>
2019-02-01 15:30:29 +0000

    Upload documentation for master

---
+++ master/doc/implementors/core/iter/traits/collect/trait.IntoIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["pyo3"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for &amp;'a <a class=\"struct\" href=\"pyo3/types/struct.PyDict.html\" title=\"struct pyo3::types::PyDict\">PyDict</a>",synthetic:false,types:["pyo3::types::dict::PyDict"]},{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for &amp;'a <a class=\"struct\" href=\"pyo3/types/struct.PyList.html\" title=\"struct pyo3::types::PyList\">PyList</a>",synthetic:false,types:["pyo3::types::list::PyList"]},{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for &amp;'a <a class=\"struct\" href=\"pyo3/types/struct.PyTuple.html\" title=\"struct pyo3::types::PyTuple\">PyTuple</a>",synthetic:false,types:["pyo3::types::tuple::PyTuple"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
