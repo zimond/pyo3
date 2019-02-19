d7db913a41fd5dc5a383b6cfc04816aff62292bc
Travis CI User <travis@example.org>
2019-02-19 21:49:28 +0000

    Upload documentation for v0.5.4

---
+++ v0.5.4/doc/pyo3/class/methods/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["PyMethodDefType","`PyMethodDefType` represents different types of python callable objects. It is used by `#[pymethods]` and `#[pyproto]` annotations."],["PyMethodType",""]],"struct":[["PyGetterDef",""],["PyMethodDef",""],["PySetterDef",""]]});
---
+++ v0.5.4/doc/pyo3/class/sequence/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"trait":[["PySequenceConcatProtocol",""],["PySequenceContainsProtocol",""],["PySequenceDelItemProtocol",""],["PySequenceGetItemProtocol",""],["PySequenceInplaceConcatProtocol",""],["PySequenceInplaceRepeatProtocol",""],["PySequenceLenProtocol",""],["PySequenceProtocol","Sequece interface"],["PySequenceRepeatProtocol",""],["PySequenceSetItemProtocol",""]]});
