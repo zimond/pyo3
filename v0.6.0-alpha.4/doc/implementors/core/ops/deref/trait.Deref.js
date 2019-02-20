e73a4b4cffc54e05fa62c0d729cd308c250f4f1b
Travis CI User <travis@example.org>
2019-02-20 10:42:22 +0000

    Upload documentation for v0.6.0-alpha.4

---
+++ v0.6.0-alpha.4/doc/implementors/core/ops/deref/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["pyo3"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"pyo3/ffi/struct.PyDateTimeAPI.html\" title=\"struct pyo3::ffi::PyDateTimeAPI\">PyDateTimeAPI</a>",synthetic:false,types:["pyo3::ffi::datetime::PyDateTimeAPI"]},{text:"impl&lt;'a, T:&nbsp;<a class=\"trait\" href=\"pyo3/typeob/trait.PyTypeInfo.html\" title=\"trait pyo3::typeob::PyTypeInfo\">PyTypeInfo</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"pyo3/struct.PyRef.html\" title=\"struct pyo3::PyRef\">PyRef</a>&lt;'a, T&gt;",synthetic:false,types:["pyo3::instance::PyRef"]},{text:"impl&lt;'a, T:&nbsp;<a class=\"trait\" href=\"pyo3/typeob/trait.PyTypeInfo.html\" title=\"trait pyo3::typeob::PyTypeInfo\">PyTypeInfo</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"pyo3/struct.PyRefMut.html\" title=\"struct pyo3::PyRefMut\">PyRefMut</a>&lt;'a, T&gt;",synthetic:false,types:["pyo3::instance::PyRefMut"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ v0.6.0-alpha.4/doc/implementors/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["pyo3"] = [{text:"impl&lt;'a, T:&nbsp;<a class=\"trait\" href=\"pyo3/typeob/trait.PyTypeInfo.html\" title=\"trait pyo3::typeob::PyTypeInfo\">PyTypeInfo</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"pyo3/struct.PyRefMut.html\" title=\"struct pyo3::PyRefMut\">PyRefMut</a>&lt;'a, T&gt;",synthetic:false,types:["pyo3::instance::PyRefMut"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
