94d4f1c2ca6fe7dcf36c13186f28396c13526480
Travis CI User <travis@example.org>
2019-10-08 03:21:03 +0000

    Upload documentation for v0.8.1

---
+++ v0.8.1/doc/implementors/core/ops/index/trait.Index.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["pyo3"] = [{text:"impl&lt;I:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/slice/trait.SliceIndex.html\" title=\"trait core::slice::SliceIndex\">SliceIndex</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.slice.html\">[</a><a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a><a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.slice.html\">]</a>&gt;&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;I&gt; for <a class=\"struct\" href=\"pyo3/types/struct.PyBytes.html\" title=\"struct pyo3::types::PyBytes\">PyBytes</a>",synthetic:false,types:["pyo3::types::string::PyBytes"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
