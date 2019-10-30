250534d9f087fe9d6a8746ec1fed427fc1911603
乌戈 <zhuoxian.dzx@alibaba-inc.com>
2019-10-30 17:50:22 +0800

    remove rebuild logic

--- build.rs
+++ build.rs
@@ -645,11 +645,5 @@ fn main() -> Result<(), String> {
         // TODO: Find out how we can set -undefined dynamic_lookup here (if this is possible)
     }
 
-    let env_vars = ["LD_LIBRARY_PATH", "PATH", "PYTHON_SYS_EXECUTABLE", "LIB"];
-
-    for var in env_vars.iter() {
-        println!("cargo:rerun-if-env-changed={}", var);
-    }
-
     Ok(())
 }
